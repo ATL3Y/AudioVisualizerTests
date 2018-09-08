fb3080a6c0df06ad8b0f8b8b91a6866b6251022a
LAPTOP-KISKEKM8\atley <aloughri@usc.edu>
2018-09-07 18:49:35 -0700

    Added moving gameobject with mic data amplitude functionality.

---
+++ Assets/AudioClipAmpTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent ( typeof ( AudioSource ) )]
+public class AudioClipAmpTest : MonoBehaviour
+{
+
+    private AudioSource aud;
+    private float updateStep = 0.1f;
+    private int sampleDataLength = 1024;
+
+    private float currentUpdateTime = 0f;
+
+    private float clipLoudness;
+    public float ClipLoudness
+    {
+        get { return clipLoudness; }
+    }
+    private float[] clipSampleData;
+
+    private void Start ( )
+    {
+        aud = GetComponent<AudioSource> ( );
+        clipSampleData = new float [ sampleDataLength ];
+
+    }
+
+    private void Update ( )
+    {
+
+        currentUpdateTime += Time.deltaTime;
+        if ( currentUpdateTime >= updateStep )
+        {
+            currentUpdateTime = 0f;
+            aud.clip.GetData ( clipSampleData, aud.timeSamples ); //I read 1024 samples, which is about 80 ms on a 44khz stereo clip, beginning at the current sample position of the clip.
+            clipLoudness = 0f;
+            foreach ( var sample in clipSampleData )
+            {
+                clipLoudness += Mathf.Abs ( sample );
+            }
+            clipLoudness /= sampleDataLength; //clipLoudness is what you are looking for
+        }
+
+    }
+}
---
+++ Assets/MicData.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent ( typeof ( AudioSource ) )]
+public class MicData : MonoBehaviour
+{
+    private string mic;
+    private AudioSource aud;
+
+    void Start ( )
+    {
+        foreach ( string device in Microphone.devices )
+        {
+            Debug.Log ( "Name: " + device );
+        }
+
+        if ( Microphone.devices.Length > 0 )
+        {
+            mic = Microphone.devices [ 0 ];
+        }
+
+        aud = GetComponent<AudioSource> ( );
+        aud.loop = true;
+        aud.playOnAwake = false;
+        aud.clip = Microphone.Start ( mic, true, 1, 44100 );
+
+        // aud.Play ( );
+    }
+
+    void Update ( )
+    {
+        if ( Input.GetKey ( KeyCode.Space ) )
+        {
+            Microphone.End ( mic );
+
+
+        }
+
+    }
+}
